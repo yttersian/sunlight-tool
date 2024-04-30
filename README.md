6fe1e4be9962ef5b789948b7c2d32f491d9ee279
Torbjorn Yttersian <torbjorn.yttersian@siemens.com>
2024-04-30 13:21:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# sunlight
+
+This simple sunlight tool script calculates the days last solstice, the equivalent day (in sunlight hours) from the period leading up to it, and the next solstice or equinox event.
+
+### Dependencies
+
+- Python 3.x
+- `ephem` library
+
+### Example output
+
+```
+66 days since solstice (equivalent of 16 October 2023)
+Next equinox in 22 days (20 March 2024)
+```
---
+++ sunlight.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import ephem
+
+
+def get_last_solstice() -> datetime:
+    today = ephem.now()
+    last_solstice = ephem.previous_solstice(today)
+    return ephem.localtime(last_solstice)
+
+def get_next_event() -> tuple[str, datetime]:
+    today = ephem.now()
+    next_equinox = ephem.next_equinox(today)
+    next_solstice = ephem.next_solstice(today)
+
+    if next_equinox < next_solstice:
+        return ("equinox", ephem.localtime(next_equinox))
+    else:
+        return ("solstice", ephem.localtime(next_solstice))
+
+
+last_soltice = get_last_solstice()
+now = datetime.now()
+days_since_solstice = now - last_soltice
+equivalent_daylight_date = last_soltice - days_since_solstice
+
+print(
+    f"{days_since_solstice.days} days since solstice (equivalent of {equivalent_daylight_date.strftime('%d %B %Y')})"
+)
+
+next_event_type, next_event_date = get_next_event()
+next_event_diff = next_event_date - now
+print(
+    f"Next {next_event_type} in {next_event_diff.days} days ({next_event_date.strftime('%d %B %Y')})"
+)
